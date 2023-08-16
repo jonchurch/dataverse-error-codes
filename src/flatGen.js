f14153200390e74a674f09a5d86132a6568ad6ef
Jon Church <me@jonchurch.com>
2023-08-15 20:23:43 -0400

    generate an src/index.js file of named exports

---
+++ src/flatGen.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
+const FLAT_ERROR_DATA = require('./flat.json')
+
+let namedExports = '';
+
+for (const [code, variableName] of Object.entries(FLAT_ERROR_DATA)) {
+  // Generate named export
+  namedExports += `export const ${variableName} = "${code}";\n`;
+}
+
+fs.writeFileSync(path.join(__dirname,'./index.js'), namedExports);
