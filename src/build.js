090854c620ec24c632d7f18ae53c5560835c820f
Jon Church <me@jonchurch.com>
2023-08-15 20:53:42 -0400

    simplify build.js, write named exports to index.js

--- src/build.js
+++ src/build.js
@@ -3,16 +3,12 @@ const path = require('path')
 
 const ALL_CODES = require('./data/errorCodes.json')
 
-function flatGen() {
-  let namedExports = '';
+let namedExports = '';
 
-  for (const code of Object.keys(ALL_CODES)) {
-    const { variableName } = ALL_CODES[code]
-    // Generate named export
-    namedExports += `export const ${variableName} = "${code}";\n`;
-  }
-
-  fs.writeFileSync(path.join(__dirname,'./lib.js'), namedExports);
+for (const code of Object.keys(ALL_CODES)) {
+  const { variableName } = ALL_CODES[code]
+  // Generate named export
+  namedExports += `export const ${variableName} = "${code}";\n`;
 }
 
-flatGen()
+fs.writeFileSync(path.join(__dirname,'./index.js'), namedExports);
